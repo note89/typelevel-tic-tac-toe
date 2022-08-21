564ea43b60004f8c9042a87e245494df91628254
Nils Eriksson <nilseriksson89.ne@gmail.com>
2022-08-21 21:15:14 +0100

    el consturctor

--- tic-tac-toe.ts
+++ tic-tac-toe.ts
@@ -40,9 +40,10 @@ type Expect<T extends true> = T;
 
 // CartesianProduct<X,Y>
 // Example: 
-// CartesianProduct<"a" | "b", "c" | "d"> = ["ac", "ad", "bc", "bd"]
+// CartesianProduct<"a" | "b", "c" | "d"> = ["a,c", "a,d", "b,c", "b,d"]
 type CartesianProductString<T extends ToStringableTypes,T2 extends ToStringableTypes > = `${T}${T2}`;
-type CartesianProductString2<T extends ToStringableTypes,T2 extends ToStringableTypes > = `${T},${T2}`;
+type CartesianProductString2<T extends Array<ToStringableTypes>,T2 extends Array<ToStringableTypes>> = `${T[number]}${T2[number]}`;
+//type CartesianProductString<T extends ToStringableTypes,T2 extends ToStringableTypes > = `${T},${T2}`;
 type ToStringableTypes = string | number | boolean | bigint;
 
 // UnionToIntersection<X>
@@ -82,53 +83,57 @@ type Player = Cross | Circle;
 type Square = Player | Empty;
 
 
+
 type FromToInc<From extends number, To extends number, acc extends any[] = []> = From extends PlusOne<To> ? acc : FromToInc<PlusOne<From>, To, [...acc, From]>;
 type FromToDec<From extends number, To extends number, acc extends any[] = []> = From extends MinusOne<To> ? acc : FromToDec<MinusOne<From>, To, [...acc, From]>;
+type Repeat<T extends any, N extends number, start extends number = 1, acc extends any[] = []> = start extends PlusOne<N> ? acc : Repeat<T, N, PlusOne<start>, [...acc, T]>;
+type ToUnion<T extends any[]> = T[number]
 
-type GetBoardOfSize<Size extends number> = FromToInc<1, Size>
-
+///##############################################
+// #### SELECT THE SIZE OF THE GAME YOU WANT ####
+///##############################################
 type Size = 3
-type Column2 = FromToInc<1,Size>
-type Row2    = FromToInc<1,Size>
-type Position2 = CartesianProductString2<Column2[number], Row2[number]>
+///#############################################
+
+type Constructor<Size extends number> = {
+  "rows": ToUnion<FromToInc<1,Size>>,
+  "columns": ToUnion<FromToInc<1,Size>>,
+  "positions": CartesianProductString2<FromToInc<1,Size>,FromToInc<1,Size>>,
+}
+
+type Gamey = Constructor<Size>;
+
+type Column = Gamey["columns"];
+type Row    = Gamey["rows"]
+type Position = Gamey["positions"];
 
 type Zip<T extends any[], U extends any[], Acc extends any[] = []> = 
   T extends [infer Head, ...infer Tail] ? 
   U extends [infer Head2, ...infer Tail2] ? 
   Zip<Tail, Tail2, [...Acc, [Head, Head2]]> : Acc  : Acc;
 
-type Diagonals<Size extends number> = Zip<FromTo<1, Size>, FromTo<1,Size>> | Zip<FromTo<1, Size>, FromTo<1,Size>>
 
-type A9 = Zip<[1,2], [1,2]>
-
-type Diag = Diagonals<3>
-
-
-
-type Column  = "Left" | "Middle" | "Right";
-type Row     = "Top"  | "Middle" | "Bottom";
-
-type Position = CartesianProductString<Row,Column>;
+type Concat<T extends [number, number][]> = {[Key in keyof T]: `${T[Key][0]}${T[Key][1]}`};
+type Diagonals<Size extends number> =  Zip<FromToInc<1, Size>, FromToInc<1,Size>> | Zip<FromToInc<1, Size>, FromToDec<Size, 1>>
+type Rows<RowNum extends number> =  RowNum extends number ? Zip<Repeat<RowNum,Size>,FromToInc<1,Size>> : never
 
 
 type Squares = { [s in Position]: Square };
 
 
-type GetRowPositions<R extends Row> = CartesianProductString<R,Column>;
-type GetColumnPositions<C extends Column> = CartesianProductString<Row,C>;
+type GetRowPositions<R extends Row> = R extends Row ? [CartesianProductString<R,Column>] : never;
+type GetColumnPositions<C extends Column> = C extends Column ? [CartesianProductString<Row,C>] : never;
+
+
+type A10 = GetRowPositions<Row>
 
 type WinningPositions =
   // Rows
-  | [GetRowPositions<"Top">]
-  | [GetRowPositions<"Middle">]
-  | [GetRowPositions<"Bottom">]
+  | GetRowPositions<Row>
   // Columns
-  | [GetColumnPositions<"Left">]
-  | [GetColumnPositions<"Middle">]
-  | [GetColumnPositions<"Right">]
+  | GetColumnPositions<Column>
   // Diagonals
-  | ["TopLeft" | "MiddleMiddle" | "BottomRight"]
-  | ["BottomLeft" | "MiddleMiddle" | "TopRight"]
+  | Concat<Diagonals<Size>>
 
 type LookupPositions<Positions extends Array<Position>, S extends Squares> =
   { [Key in keyof Positions ]: S[Positions[Key]] }
@@ -168,10 +173,10 @@ interface Board<
 
 interface Draw<B extends Board<any, any, any>> extends HasPrevious<B> {}
 
-
 type InitialSquares = { [key in keyof Squares]: Empty };
 
 type InitialBoard = Board<InitialSquares, Cross, Nil>;
+
 
 type AvailableSquares<B extends Squares> = {
   [key in keyof B]: B[key] extends Empty ? key : never;
@@ -265,22 +270,22 @@ type DrawString = ReturnType<WhoWonOrDraw<DrawFinal>>;
 // No double moves by same player
 // prettier-ignore
 // @ts-expect-error
-type NoDoubleMove = Move<Move<InitialBoard, Cross, "TopRight">, Cross, "TopLeft" >;
+type NoDoubleMove = Move<Move<InitialBoard, Cross, "33">, Cross, "13" >;
 
 // No start with Circle
 // @ts-expect-error
-type NoStartWithCircle = Move<InitialBoard, Circle, "TopRight">;
+type NoStartWithCircle = Move<InitialBoard, Circle, "33">;
 
 // No taking a used square
 // prettier-ignore
 // @ts-expect-error
-type NoTakingAUsedSquare = Move<Move<InitialBoard, Cross, "TopRight">,Circle, "TopRight">
+type NoTakingAUsedSquare = Move<Move<InitialBoard, Cross, "33">,Circle, "33">
 
 // Don't take to many moves back
 // @ts-expect-error
 type NotToManyMovesBack = TakeMoveBack<InitialBoard>;
 
-type MoveOne = Move<InitialBoard, Cross, "TopRight">;
+type MoveOne = Move<InitialBoard, Cross, "33">;
 type BackToNormal = TakeMoveBack<MoveOne>;
 type Outcome = Expect<Equal<InitialBoard, BackToNormal>>;
 
@@ -290,45 +295,45 @@ type BackFromWin = Expect<Equal<TakeMoveBack<WinCrossFinal>, WinCrossStep4>>;
 // IsPositionOccupied
 
 type Test = Expect<
-  Equal<IsPositionOccupied<WinCrossFinal, "MiddleLeft">, false>
+  Equal<IsPositionOccupied<WinCrossFinal, "12">, false>
 >;
-type Test2 = Expect<Equal<IsPositionOccupied<WinCrossFinal, "TopRight">, true>>;
-type Test3 = Expect<Equal<IsPositionOccupied<InitialBoard, "TopRight">, false>>;
+type Test2 = Expect<Equal<IsPositionOccupied<WinCrossFinal, "33">, true>>;
+type Test3 = Expect<Equal<IsPositionOccupied<InitialBoard, "33">, false>>;
 // @ts-expect-error
-type Test4 = Expect<Equal<IsPositionOccupied<InitialBoard, "TopRight">, true>>;
+type Test4 = Expect<Equal<IsPositionOccupied<InitialBoard, "33">, true>>;
 
 // DRAW GAME
 
-type DrawStep1 = Move<InitialBoard, Cross, "TopLeft">;
-type DrawStep2 = Move<DrawStep1, Circle, "TopMiddle">;
-type DrawStep3 = Move<DrawStep2, Cross, "TopRight">;
-type DrawStep4 = Move<DrawStep3, Circle, "MiddleLeft">;
-type DrawStep5 = Move<DrawStep4, Cross, "MiddleMiddle">;
-type DrawStep6 = Move<DrawStep5, Circle, "BottomLeft">;
-type DrawStep7 = Move<DrawStep6, Cross, "MiddleRight">;
-type DrawStep8 = Move<DrawStep7, Circle, "BottomRight">;
-type DrawFinal = Move<DrawStep8, Cross, "BottomMiddle">;
+type DrawStep1 = Move<InitialBoard, Cross, "13">;
+type DrawStep2 = Move<DrawStep1, Circle, "23">;
+type DrawStep3 = Move<DrawStep2, Cross, "33">;
+type DrawStep4 = Move<DrawStep3, Circle, "12">;
+type DrawStep5 = Move<DrawStep4, Cross, "22">;
+type DrawStep6 = Move<DrawStep5, Circle, "11">;
+type DrawStep7 = Move<DrawStep6, Cross, "32">;
+type DrawStep8 = Move<DrawStep7, Circle, "31">;
+type DrawFinal = Move<DrawStep8, Cross, "21">;
 
 type DrawOutcome = Expect<Draw<any> extends DrawFinal ? true : false>;
 
 // Cross Win
-type WinCrossStep1 = Move<InitialBoard, Cross, "TopLeft">;
-type WinCrossStep2 = Move<WinCrossStep1, Circle, "BottomLeft">;
-type WinCrossStep3 = Move<WinCrossStep2, Cross, "TopRight">;
-type WinCrossStep4 = Move<WinCrossStep3, Circle, "BottomMiddle">;
-type WinCrossFinal = Move<WinCrossStep4, Cross, "TopMiddle">;
+type WinCrossStep1 = Move<InitialBoard, Cross, "13">;
+type WinCrossStep2 = Move<WinCrossStep1, Circle, "11">;
+type WinCrossStep3 = Move<WinCrossStep2, Cross, "33">;
+type WinCrossStep4 = Move<WinCrossStep3, Circle, "21">;
+type WinCrossFinal = Move<WinCrossStep4, Cross, "23">;
 
 type WinCrossOutcome = Expect<Equal<
    WinCrossFinal["winner"], Cross
 >>;
 
 // Circle Win
-type WinCircleStep1 = Move<InitialBoard, Cross, "TopLeft">;
-type WinCircleStep2 = Move<WinCircleStep1, Circle, "BottomLeft">;
-type WinCircleStep3 = Move<WinCircleStep2, Cross, "TopRight">;
-type WinCircleStep4 = Move<WinCircleStep3, Circle, "BottomMiddle">;
-type WinCircleStep5 = Move<WinCircleStep4, Cross, "MiddleMiddle">;
-type WinCircleFinal = Move<WinCircleStep5, Circle, "BottomRight">;
+type WinCircleStep1 = Move<InitialBoard, Cross, "13">;
+type WinCircleStep2 = Move<WinCircleStep1, Circle, "11">;
+type WinCircleStep3 = Move<WinCircleStep2, Cross, "33">;
+type WinCircleStep4 = Move<WinCircleStep3, Circle, "21">;
+type WinCircleStep5 = Move<WinCircleStep4, Cross, "22">;
+type WinCircleFinal = Move<WinCircleStep5, Circle, "31">;
 
 type WinCircleOutcome = Expect<Equal<
    WinCircleFinal["winner"], Circle
